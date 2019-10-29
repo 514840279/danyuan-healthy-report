8c3a34c2f3ee93538174e1a61113436e0b49ee98
王滕辉 <514840279@qq.com>
2019-10-30 07:30:52 +0800

    excel 上传

--- src/main/java/org/danyuan/application/healthy/daoru/service/SysFileImpInfoService.java
+++ src/main/java/org/danyuan/application/healthy/daoru/service/SysFileImpInfoService.java
@@ -12,6 +12,9 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.danyuan.application.common.base.BaseService;
 import org.danyuan.application.common.base.BaseServiceImpl;
 import org.danyuan.application.healthy.daoru.po.SysFileImpInfo;
@@ -30,10 +33,10 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
  */
 @Service
 public class SysFileImpInfoService extends BaseServiceImpl<SysFileImpInfo> implements BaseService<SysFileImpInfo> {
-	
+
 	@Autowired
 	DaoruAsyncService daoruAsyncService;
-	
+
 	/**
 	 * @throws IOException
 	 * @方法名 upload
@@ -55,25 +58,37 @@ public class SysFileImpInfoService extends BaseServiceImpl<SysFileImpInfo> imple
 			// .replace("(", "").replace(" ", "").replace(")", "");
 			InputStream inputStream = null;
 			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-			
+
 			String path = System.getProperty("user.dir") + "/fileupload/" + simpleDateFormat.format(new Date());
-			
+
 			File file = new File(path);
 			inputStream = multipartFile.getInputStream();
-			
+			// 获取excel文件的io流
+			// 根据文件后缀名不同(xls和xlsx)获得不同的Workbook实现类对象
+			Workbook workbook = null;
+			if (filename.endsWith("xls")) {
+				// xls
+				workbook = new HSSFWorkbook(inputStream);
+			} else if (filename.endsWith("xlsx")) {
+				// xlsx
+				workbook = new XSSFWorkbook(inputStream);
+			}
+
 			if (!file.exists()) {
 				file.mkdirs();
 			}
 			String newFileNameString = UUID.randomUUID().toString().replace("-", "") + ".xlsx";
 			path = path + "/" + newFileNameString;
+
 			FileOutputStream fos = new FileOutputStream(path);
-			
-			byte[] b = new byte[1024];
-			while ((inputStream.read(b)) != -1) {
-				fos.write(b);
-			}
+			workbook.write(fos);
+//			byte[] b = new byte[1024];
+//			while ((inputStream.read(b)) != -1) {
+//				fos.write(b);
+//			}
 			fos.close();
 			inputStream.close();
+
 			// 保存信息
 			SysFileImpInfo sysFileImpInfo = new SysFileImpInfo();
 			sysFileImpInfo.setFilePath("/" + simpleDateFormat.format(new Date()) + "/" + URLEncoder.encode(newFileNameString, "utf-8"));
@@ -84,11 +99,11 @@ public class SysFileImpInfoService extends BaseServiceImpl<SysFileImpInfo> imple
 			sysFileImpInfo.setFileSize(multipartFile.getSize());
 			sysFileImpInfo.setImpType(0);
 			save(sysFileImpInfo);
-
+			
 			// 执行异步倒入数据
 			daoruAsyncService.execAsync();
 		}
-
+		
 	}
-	
+
 }
